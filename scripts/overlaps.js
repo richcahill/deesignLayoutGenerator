275b248dff2764be7d427ccc502147a9e49759fb
richcahill <rich@rc3.me>
2015-07-19 18:03:55 +0100

    c
    Come on no collision here we go.

--- scripts/overlaps.js
+++ scripts/overlaps.js
@@ -1,71 +1,54 @@
-/*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
- * Licensed under the MIT License (LICENSE.txt).
- *
- * Version 1.2.3
- */
+var boxDims = new Array();
 
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function ($) {
-
-    $.fn.overlaps = function(selector) {
-        return this.pushStack(filterOverlaps(this, selector && $(selector)));
-    };
-
-    function filterOverlaps(collection1, collection2) {
-        var dims1  = getDims(collection1),
-            dims2  = !collection2 ? dims1 : getDims(collection2),
-            stack  = [],
-            index1 = 0,
-            index2 = 0,
-            length1 = dims1.length,
-            length2 = !collection2 ? dims1.length : dims2.length;
-
-        if (!collection2) { collection2 = collection1; }
-
-        for (; index1 < length1; index1++) {
-            for (index2 = 0; index2 < length2; index2++) {
-                if (collection1[index1] === collection2[index2]) {
-                    continue;
-                } else if (checkOverlap(dims1[index1], dims2[index2])) {
-                    stack.push( (length1 > length2) ?
-                        collection1[index1] :
-                        collection2[index2]);
-                }
-            }
+function setRandomPos(elements,x,dx){
+  elements.each(function(){
+    var conflict = true;
+    while (conflict) {
+        fixLeft=(Math.floor(Math.random()*x)*10) + dx;
+        fixTop = (Math.floor(Math.random()*40)*10) + 180;
+        $(this).offset({
+            left: fixLeft,
+            top: fixTop
+        });
+        var box = {
+            top: parseInt(window.getComputedStyle($(this)[0]).top),
+            left: parseInt(window.getComputedStyle($(this)[0]).left),
+            width: parseInt(window.getComputedStyle($(this)[0]).width),
+            height: parseInt(window.getComputedStyle($(this)[0]).height)
         }
-
-        return $.unique(stack);
-    }
-
-    function getDims(elems) {
-        var dims = [], i = 0, offset, elem;
-
-        while ((elem = elems[i++])) {
-            offset = $(elem).offset();
-            dims.push([
-                offset.top,
-                offset.left,
-                elem.offsetWidth,
-                elem.offsetHeight
-            ]);
+        conflict = false;
+        for (var i=0;i<boxDims.length;i++) {
+            if (overlap(box,boxDims[i])) {
+                conflict = true;
+                break;
+            } else {
+                conflict = false;
+            }                   
         }
-
-        return dims;
     }
+    boxDims.push(box)
 
-    function checkOverlap(dims1, dims2) {
-        var x1 = dims1[1], y1 = dims1[0],
-            w1 = dims1[2], h1 = dims1[3],
-            x2 = dims2[1], y2 = dims2[0],
-            w2 = dims2[2], h2 = dims2[3];
-        return !(y2 + h2 <= y1 || y1 + h1 <= y2 || x2 + w2 <= x1 || x1 + w1 <= x2);
-    }
+  });
+}
 
-}));
+function overlap(box1,box2) {
+  var x1 = box1.left
+  var y1 = box2.top;
+  var h1 = box1.height;
+  var w1 = box1.width;
+  var b1 = y1 + h1;
+  var r1 = x1 + w1;
+  var x2 = box1.left;
+  var y2 = box1.top;
+  var h2 = box1.height;
+  var w2 = box1.width;
+  var b2 = y2 + h2;
+  var r2 = x2 + w2;
+
+  var buf = 24;
+
+  if (b1 + buf < y2 || y1 > b2 + buf || r1 + buf < x2 || x1 > r2 + buf) return false;
+  return true;
+}
+
+setRandomPos($(".boxes"),40,40);
